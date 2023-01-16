839b0c4d01710e72777db46c4049479d4234c491
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-01-16 22:06:12 +0100

    new js

--- web/main.js
+++ web/main.js
@@ -1,27 +1,27 @@
-const form = document.querySelector('form')
-const button = form.querySelector('input'[type="submit"])
-const msg = "Please provide a valid email address";
+const form = document.querySelector("form")
+const button = form.querySelector('[type="submit"]')
 
-const error = document.createElement('p');
-error.classList.add('error_text');
-error.textContent = msg;
+const msg = "Please provide a valid email address"
+const error = document.createElement('p')
+error.classList.add('error_text')
+error.textContent = msg
 
-function checkEmail(e) {
-    e.preventDefault();
-    const email = e.target.querySelector('[type="email"]').value;
-    if(!validateEmail(email)) {
+function checkEmail(event) {
+    event.preventDefault()
+    const email= event.target.querySelector('[type="email"]').value
+    console.log(email)
+    if (!validateEmail(email)) {
         form.classList.add('error')
         form.insertBefore(error, button)
     } else {
+        form.removeChild('error')
         form.classList.remove('error')
-        form.removeChild(error)
     }
-    console.log(email)
 }
 
 function validateEmail (email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase())
+    const regexKey = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexKey.test(String(email).toLowerCase())
 }
 
 form.addEventListener('submit', checkEmail)
